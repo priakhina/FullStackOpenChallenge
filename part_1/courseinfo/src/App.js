f080f3a6ceffe530d280579b327e2f775a57e187
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-07-10 14:55:32 -0600

    Add solution for exercise 1.4 (courseinfo, step 4)

--- part_1/courseinfo/src/App.js
+++ part_1/courseinfo/src/App.js
@@ -1,41 +1,39 @@
 const App = () => {
     const course = "Half Stack application development";
-    const part1 = {
-        name: "Fundamentals of React",
-        exercises: 10,
-    };
-    const part2 = {
-        name: "Using props to pass data",
-        exercises: 7,
-    };
-    const part3 = {
-        name: "State of a component",
-        exercises: 14,
-    };
+    const parts = [
+        {
+            name: "Fundamentals of React",
+            exercises: 10,
+        },
+        {
+            name: "Using props to pass data",
+            exercises: 7,
+        },
+        {
+            name: "State of a component",
+            exercises: 14,
+        },
+    ];
 
     return (
         <div>
-            <Header courseName={course} />
-            <Content part1={part1} part2={part2} part3={part3} />
-            <Total
-                totalNumberOfExercises={
-                    part1.exercises + part2.exercises + part3.exercises
-                }
-            />
+            <Header course={course} />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
     );
 };
 
-const Header = ({ courseName }) => {
-    return <h1>{courseName}</h1>;
+const Header = ({ course }) => {
+    return <h1>{course}</h1>;
 };
 
-const Content = ({ part1, part2, part3 }) => {
+const Content = ({ parts }) => {
     return (
         <div>
-            <Part name={part1.name} numberOfExercises={part1.exercises} />
-            <Part name={part2.name} numberOfExercises={part2.exercises} />
-            <Part name={part3.name} numberOfExercises={part3.exercises} />
+            <Part name={parts[0].name} numberOfExercises={parts[0].exercises} />
+            <Part name={parts[1].name} numberOfExercises={parts[1].exercises} />
+            <Part name={parts[2].name} numberOfExercises={parts[2].exercises} />
         </div>
     );
 };
@@ -48,8 +46,13 @@ const Part = ({ name, numberOfExercises }) => {
     );
 };
 
-const Total = ({ totalNumberOfExercises }) => {
-    return <p>Number of exercises {totalNumberOfExercises}</p>;
+const Total = ({ parts }) => {
+    return (
+        <p>
+            Number of exercises{" "}
+            {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+        </p>
+    );
 };
 
 export default App;
