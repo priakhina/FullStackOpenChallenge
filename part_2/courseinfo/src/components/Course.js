ddfa7769777d77a79bad18bd62735ef11997f554
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-07-12 18:20:58 -0600

    Add solution for exercises 2.2-2.3 (courseinfo, step 7 & step 8)

--- part_2/courseinfo/src/components/Course.js
+++ part_2/courseinfo/src/components/Course.js
@@ -1,11 +1,18 @@
 import Header from "./Header";
 import Content from "./Content";
+import Total from "./Total";
 
 const Course = ({ course }) => {
     return (
         <>
             <Header text={course.name} />
             <Content parts={course.parts} />
+            <Total
+                sum={course.parts.reduce(
+                    (total, part) => total + part.exercises,
+                    0
+                )}
+            />
         </>
     );
 };
---
+++ part_2/courseinfo/src/components/Total.js
@@ -0,0 +1,7 @@
+const Total = ({ sum }) => (
+    <p>
+        <strong>Total of {sum} exercises</strong>
+    </p>
+);
+
+export default Total;
