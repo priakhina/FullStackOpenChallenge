57b55d75521497aeb716b775df095c336c800804
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-07-28 15:48:32 -0600

    Move the actual Express application to the app.js file for exercise 4.2 (blog list, step 2)

--- part_4/blog-list/index.js
+++ part_4/blog-list/index.js
@@ -1,25 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const blogsRouter = require("./controllers/blogs");
+const app = require("./app"); // the actual Express application
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-const middleware = require("./utils/middleware");
 
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-app.use("/api/blogs", blogsRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-const PORT = config.PORT;
-app.listen(PORT, () => {
-	logger.info(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+	logger.info(`Server running on port ${config.PORT}`);
 });
