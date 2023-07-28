f8f7e38571a1f21d75e13d015f228b2d619e44fc
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-07-28 15:27:37 -0600

    Refactor the requestLogger to use the logger module for printing logs

--- part_4/blog-list/index.js
+++ part_4/blog-list/index.js
@@ -13,10 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 const requestLogger = (request, response, next) => {
-	console.log("Method:", request.method);
-	console.log("Path:  ", request.path);
-	console.log("Body:  ", request.body);
-	console.log("---");
+	logger.info("Method:", request.method);
+	logger.info("Path:  ", request.path);
+	logger.info("Body:  ", request.body);
+	logger.info("---");
 	next();
 };
 
