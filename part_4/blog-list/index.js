c11b13baa5f9cb8624aef86c25e5e03d45903935
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-07-28 14:54:00 -0600

    Extract the Mongoose-specific code into a dedicated module for exercise 4.2 (blog list, step 2)

--- part_4/blog-list/index.js
+++ part_4/blog-list/index.js
@@ -5,14 +5,7 @@ const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 
-const blogSchema = new mongoose.Schema({
-	title: String,
-	author: String,
-	url: String,
-	likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blog");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
