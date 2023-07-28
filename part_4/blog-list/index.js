431885f7cd7d5ad61a242a75d713706c4ef36a08
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-07-28 15:36:05 -0600

    Extract custom middleware into a dedicated module for exercise 4.2 (blog list, step 2)

--- part_4/blog-list/index.js
+++ part_4/blog-list/index.js
@@ -5,41 +5,19 @@ const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
+const middleware = require("./utils/middleware");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
-
-const requestLogger = (request, response, next) => {
-	logger.info("Method:", request.method);
-	logger.info("Path:  ", request.path);
-	logger.info("Body:  ", request.body);
-	logger.info("---");
-	next();
-};
-
-app.use(requestLogger);
+app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 
-const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: "unknown endpoint" });
-};
-
-const errorHandler = (error, request, response, next) => {
-	logger.error(error.message);
-
-	if (error.name === "CastError") {
-		return response.status(400).send({ error: "malformed id" });
-	}
-
-	next(error);
-};
-
-app.use(unknownEndpoint);
-app.use(errorHandler);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
