259b3f578cea6b7cde026473e2875a843abf6b20
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-08-01 10:29:02 -0600

    Eliminate the try-catch statements in async express route callbacks by using the express-async-errors library

--- part_4/blog-list/controllers/blogs.js
+++ part_4/blog-list/controllers/blogs.js
@@ -6,15 +6,11 @@ blogsRouter.get("/", async (request, response) => {
 	response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
 	const blog = new Blog(request.body);
 
-	try {
-		const savedBlog = await blog.save();
-		response.status(201).json(savedBlog);
-	} catch (exception) {
-		next(exception);
-	}
+	const savedBlog = await blog.save();
+	response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
