65ffbf0ff8d651b5fcefc987881d4b9302325456
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-08-18 11:15:20 -0600

    Add solution for exercise 6.2 (unicafe revisited, step 2)

--- part_6/unicafe-redux/src/index.js
+++ part_6/unicafe-redux/src/index.js
@@ -7,21 +7,20 @@ import reducer from "./reducer";
 const store = createStore(reducer);
 
 const App = () => {
-	const good = () => {
-		store.dispatch({
-			type: "GOOD",
-		});
-	};
+	const incrementGood = () => store.dispatch({ type: "GOOD" });
+	const incrementOk = () => store.dispatch({ type: "OK" });
+	const incrementBad = () => store.dispatch({ type: "BAD" });
+	const resetStats = () => store.dispatch({ type: "ZERO" });
 
 	return (
 		<div>
-			<button onClick={good}>good</button>
-			<button>ok</button>
-			<button>bad</button>
-			<button>reset stats</button>
-			<div>good {store.getState().good}</div>
-			<div>ok</div>
-			<div>bad</div>
+			<button onClick={incrementGood}>good</button>
+			<button onClick={incrementOk}>ok</button>
+			<button onClick={incrementBad}>bad</button>
+			<button onClick={resetStats}>reset stats</button>
+			<div>good: {store.getState().good}</div>
+			<div>ok: {store.getState().ok}</div>
+			<div>bad: {store.getState().bad}</div>
 		</div>
 	);
 };
