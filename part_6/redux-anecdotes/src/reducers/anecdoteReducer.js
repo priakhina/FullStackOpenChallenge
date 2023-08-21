fb90ec6222f9ccff7c2fdc79aaccbca64809ecbc
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-08-21 09:45:49 -0600

    Add solution for exercise 6.16 (anecdotes and the backend, step 3)

--- part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const anecdoteSlice = createSlice({
 	name: "anecdotes",
@@ -23,6 +24,13 @@ const anecdoteSlice = createSlice({
 		},
 	},
 });
+
+export const initializeAnecdotes = () => {
+	return async (dispatch) => {
+		const anecdotes = await anecdoteService.getAll();
+		dispatch(setAnecdotes(anecdotes));
+	};
+};
 
 export const { voteAnecdote, createAnecdote, setAnecdotes } =
 	anecdoteSlice.actions;
