953e137cb0606f8f83ce7bc4c8622091fce8be66
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2024-02-07 18:56:00 -0700

    Refactor solution for exercise 9.1

--- part_9/ex_9.1-9.7/bmiCalculator.ts
+++ part_9/ex_9.1-9.7/bmiCalculator.ts
@@ -1,27 +1,29 @@
-const calculateBmi = (heightInCm: number, weightInKg: number): string => {
+const calculateBmi = (heightInCm: number, weightInKg: number): number => {
   const bmi = weightInKg / Math.pow(heightInCm / 100, 2);
-  const roundedBmi = Math.round(bmi * 10) / 10; // rounding to one decimal place
+  return Math.round(bmi * 10) / 10; // rounding to one decimal place
+};
 
+const defineBmiCategory = (bmi: number): string => {
   switch (true) {
-    case roundedBmi < 16:
+    case bmi < 16:
       return 'Underweight (Severe thinness)';
-    case roundedBmi <= 16.9:
+    case bmi <= 16.9:
       return 'Underweight (Moderate thinness)';
-    case roundedBmi <= 18.4:
+    case bmi <= 18.4:
       return 'Underweight (Mild thinness)';
-    case roundedBmi <= 24.9:
+    case bmi <= 24.9:
       return 'Normal (Healthy weight)';
-    case roundedBmi <= 29.9:
+    case bmi <= 29.9:
       return 'Overweight (Pre-obese)';
-    case roundedBmi <= 34.9:
+    case bmi <= 34.9:
       return 'Obese (Class I)';
-    case roundedBmi <= 39.9:
+    case bmi <= 39.9:
       return 'Obese (Class II)';
-    case roundedBmi >= 40:
+    case bmi >= 40:
       return 'Obese (Class III)';
     default:
       break;
   }
 };
 
-console.log(calculateBmi(180, 74));
+console.log(defineBmiCategory(calculateBmi(180, 74)));
